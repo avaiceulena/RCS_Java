c2f3ebb04f71f90143facc1ecf90e84b1915edf8
avaiceulena <avaiceulena@gmail.com>
2023-11-14 20:36:55 +0200

    Update Main.java

--- Day3_loops/src/main/java/org/example/Main.java
+++ Day3_loops/src/main/java/org/example/Main.java
@@ -6,7 +6,57 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        task2();
+        task3();
+    }
+
+    public static void task3(){
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            System.out.println("Lūdzu ievadiet skaitli!");
+            int number = sc.nextInt();
+
+            if(number > 0){
+                sum = sum + number;
+            }else{
+                System.out.println("Kļūda!");
+            }
+
+        }
+        System.out.println("Summa ir " + sum);
+    }
+
+    public static void sample4(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Lūdzu ievadiet skaitli!");
+        int number = sc.nextInt();
+
+        if(number == 5){
+            for(int i = 0; i < 5; i++){
+                System.out.println("hello");
+            }
+        }else{
+            System.out.println("Kļūda");
+        }
+    }
+
+    public static void sample3(){
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Lūdzu ievadiet skaitli!");
+            int number = sc.nextInt();
+
+            if(number == 0){
+                System.out.println("Kļūda");
+            }else if(number == 4){
+                System.out.println("Hello world!");
+            }else{
+                System.out.println("Hello world!2");
+            }
+        }
     }
 
     public static void task2(){
@@ -16,12 +66,14 @@ public class Main {
        // int b = a.length();
 
 
-            int sum = 0;
+        int sum = 0;
         for(int i = 0; i < 5; i++){
             System.out.println("Ievadi string tipa vērtības!");
             String c = sc.nextLine();
             int b = c.length();
             sum = sum + b;
+
+            // pareizāk sum = sum + c.length;
 
         }
         System.out.println("summa ir " + sum);
@@ -30,8 +82,8 @@ public class Main {
     public static void sample2(){
         Scanner sc = new Scanner(System.in);
 
-        String a = "aaa";
-        int b = a.length();
+        //String a = "aaa";
+       // int b = a.length();
 
 
         int sum = 0;
