15ca775142e7292a513144d5669fc0571a3cf2ac
avaiceulena <avaiceulena@gmail.com>
2023-11-20 20:12:52 +0200

    Update Main.java

--- Day4_classes/src/main/java/org/example/Main.java
+++ Day4_classes/src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ public class Main {
     }
 
     public static void sample2(int a){
-        if(a > 1){
+        if(a >= 1){
             for(int i = 0; i <= a; i++){
                 for(int j = 1; j <= i; j++){
                     System.out.print("&");
@@ -25,7 +25,7 @@ public class Main {
         }
     }
 
- 
+
     public static void  sampleWith(String value1, String value2, String value3 ){
        // System.out.println(value3 + value2 + value1);
 
