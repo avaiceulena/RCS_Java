987e0fee6741962f177eedad2ee9901f24f8bb00
avaiceulena <avaiceulena@gmail.com>
2023-11-20 18:53:22 +0200

    Day 4

---
+++ Day4_classes/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String value1 = "aaa";
+        String value2 = "bbb";
+        String value3 = "ccc";
+
+        sampleWith(value1, value2, value3);
+    }
+    public static void  sampleWith(String value1, String value2, String value3 ){
+        System.out.println(value3 + value2 + value1);
+    }
+}
