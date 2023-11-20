4519092b3e00dd4b0ca6330dd74553754351d9f2
avaiceulena <avaiceulena@gmail.com>
2023-11-20 20:04:43 +0200

    Update Main.java

--- Day4_classes/src/main/java/org/example/Main.java
+++ Day4_classes/src/main/java/org/example/Main.java
@@ -2,11 +2,39 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String value1 = "aaa";
-        String value2 = "bbb";
-        String value3 = "ccc";
+        // String value1 = "aaa";
+        // String value2 = "bbb";
+        //String value3 = "ccc";
 
-        sampleWith(value1, value2, value3);
+        //sampleWith(value1, value2, value3);
+
+        sample2(0);
+
+
+
+    }
+
+    public static void sample2(int a){
+        if(a > 1){
+            for(int i = 0; i <= a; i++){
+                for(int j = 1; j <= i; j++){
+                    System.out.print("&");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("$");
+        }
+    }
+
+    public static void MD1() {
+
+        for (int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++){
+                System.out.print("$");
+            }
+            System.out.println();
+        }
     }
     public static void  sampleWith(String value1, String value2, String value3 ){
        // System.out.println(value3 + value2 + value1);
