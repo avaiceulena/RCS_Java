4b67abbc12223272ed873e2fc21b4d8f3c6a0e32
avaiceulena <avaiceulena@gmail.com>
2023-11-20 19:08:19 +0200

    Update Main.java

--- Day4_classes/src/main/java/org/example/Main.java
+++ Day4_classes/src/main/java/org/example/Main.java
@@ -9,6 +9,14 @@ public class Main {
         sampleWith(value1, value2, value3);
     }
     public static void  sampleWith(String value1, String value2, String value3 ){
-        System.out.println(value3 + value2 + value1);
+       // System.out.println(value3 + value2 + value1);
+
+        int len = value3.length() + value2.length() + value1.length();
+        if(len > 15){
+            System.out.println(value1 + value3);
+        }else {
+            System.out.println(value1 + value2 + value3);
+        }
+
     }
 }
