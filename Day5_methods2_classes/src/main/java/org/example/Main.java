f7102246129e93a8fc66bb93504a3c635e779236
avaiceulena <avaiceulena@gmail.com>
2023-11-21 21:31:22 +0200

    day 5

--- Day5_methods2_classes/src/main/java/org/example/Main.java
+++ Day5_methods2_classes/src/main/java/org/example/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
        int c =  task2(1, 4);
        System.out.println(c);
-    }
+       // vai System.out.println(task2(1, 4));
 
+        Samples.sample1();
+
+        System.out.println(Tasks.biggestNumber(6));
+
+
+    }
     public static int task2 (int num1, int num2){
         if(num1 > num2){
             return num1;
