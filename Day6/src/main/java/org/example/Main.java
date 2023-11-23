f3aeb9717afd07015fb66b310e9d0fa9d87d0029
avaiceulena <avaiceulena@gmail.com>
2023-11-23 20:18:56 +0200

    Day6

--- Day6/src/main/java/org/example/Main.java
+++ Day6/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Tasks.number(2));
+       //System.out.println(Tasks.number(2));
+        Tasks.task2b();
+
+
+
     }
 }
--- Day6/src/main/java/org/example/Tasks.java
+++ Day6/src/main/java/org/example/Tasks.java
@@ -3,6 +3,30 @@ package org.example;
 import java.util.Scanner;
 
 public class Tasks {
+
+    public static void task2b(){
+        task2();
+    }
+        private static void task2() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ievadi skaitli!");
+            int input1 = sc.nextInt();
+            int last = Integer.MIN_VALUE;
+
+            while (input1 > last) {
+                System.out.println("Ievadi skaitli!");
+                int input2 = sc.nextInt();
+
+                if (input2 < input1) {
+                    System.out.println("Hello world!");
+                    input1 = input2;
+                } else {
+                    break;
+                }
+            }
+        }
+
+
     public static int number(int count){
         Scanner sc = new Scanner(System.in);
 
