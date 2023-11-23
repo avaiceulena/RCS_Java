7e2839f106dc5318f3394d4160df13033250b627
avaiceulena <avaiceulena@gmail.com>
2023-11-23 21:15:35 +0200

    Update Main.java

--- Day6/src/main/java/org/example/Main.java
+++ Day6/src/main/java/org/example/Main.java
@@ -2,11 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       //System.out.println(Tasks.number(2));
-       // Tasks.task2b();
-        // Sample.sample1();
-        //Sample.sample2();
-        //Sample.sample3();
+       System.out.println(Tasks.number(2));
+//        Tasks.task2b();
+//        Sample.sample1();
+//        Sample.sample2();
+//        Sample.sample3();
         Tasks.task3();
 
 
