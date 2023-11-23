13cb965320961c664d0a9586d3a93096137290e2
avaiceulena <avaiceulena@gmail.com>
2023-11-23 21:12:54 +0200

    day6

--- Day6/src/main/java/org/example/Main.java
+++ Day6/src/main/java/org/example/Main.java
@@ -3,7 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
        //System.out.println(Tasks.number(2));
-        Tasks.task2b();
+       // Tasks.task2b();
+        // Sample.sample1();
+        //Sample.sample2();
+        //Sample.sample3();
+        Tasks.task3();
 
 
 
--- Day6/src/main/java/org/example/Tasks.java
+++ Day6/src/main/java/org/example/Tasks.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Tasks {
 
+    public static void task3(){
+        Scanner sc = new Scanner(System.in);
+        String[] b = new String[5];
+
+        for(int i = 0; i < b.length; i++){
+            System.out.println("Ievadi vārdus");
+            b[i] = sc.nextLine();
+        }
+        for(int i = 0; i < b.length; i++){
+            int c = b[i].length();
+            System.out.println(b[i] + " " + c);
+        }
+    }
+
     public static void task2b(){
         task2();
     }
