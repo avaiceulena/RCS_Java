9d79eebea351d0bb38731eabd0ef9a2fee454b01
avaiceulena <avaiceulena@gmail.com>
2023-11-23 19:11:07 +0200

    Update Main.java

--- Day6/src/main/java/org/example/Main.java
+++ Day6/src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int c = Tasks.number(2);
-
-        System.out.println(c);
+        System.out.println(Tasks.number(2));
     }
 }
